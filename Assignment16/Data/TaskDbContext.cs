abe63ba347f266e7d280a316f818fd4a8fff860b
Sravankumar1612 <sravankumartikkavarapu@gmail.com>
2023-09-15 08:36:23 +0530

    origin master

---
+++ Assignment16/Data/TaskDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment16.Models;
+
+namespace Assignment16.Data
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext (DbContextOptions<TaskDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment16.Models.Task> Task { get; set; } = default!;
+    }
+}
